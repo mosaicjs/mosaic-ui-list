ac76499c71153991b3d0a4dcb27f4af4e97f5a53
mkotelnikov <mikhail.kotelnikov@ubimix.com>
2015-08-03 18:28:56 +0200

    Change the default visualization. Add styles.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import ListItemView from './lib/ListItemView';
 import ListView from './lib/ListView';
 import ListPaginationView from './lib/ListPaginationView';
+import ListSizeView from './lib/ListSizeView';
 
 export default {
     ListItemView,
     ListView,
-    ListPaginationView
+    ListPaginationView,
+    ListSizeView
 }
