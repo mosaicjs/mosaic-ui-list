783c82244fd40e667bf2486ece1f41aec04db617
Mikhail Kotelnikov <mikhail.kotelnikov@gmail.com>
2016-02-22 18:09:09 +0100

    Externalize ListPagination class from the ListPaginationView.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import ListItemView from './lib/ListItemView';
 import ListView from './lib/ListView';
+import ListPagination from './lib/ListPagination';
 import ListPaginationView from './lib/ListPaginationView';
 import ListSizeView from './lib/ListSizeView';
 
 export default {
     ListItemView,
     ListView,
+    ListPagination,
     ListPaginationView,
     ListSizeView
 }
